cae285f88ac85ddb309909ed5b73f732cf05a98a
Nicolae Hodos <nicu.hodos@gmail.com>
2016-04-28 15:02:36 +0200

    read distance twice for better accuracy

--- SonarSensor.h
+++ SonarSensor.h
@@ -2,6 +2,15 @@ class SonarSensor {
   byte trigger;
   byte echo;
   long distance;
+
+  long readDistance() {
+    digitalWrite(trigger, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigger, LOW);
+    return pulseIn(echo, HIGH)/58;
+  }
 public:
   SonarSensor(byte trigger, byte echo) {
     this->trigger = trigger;
@@ -11,12 +20,11 @@ public:
   }
 
   void sendSignal() {
-    digitalWrite(trigger, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigger, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigger, LOW);
-    distance = pulseIn(echo, HIGH)/58;
+    long distance1 = readDistance();
+    distance = readDistance();
+    if (abs(distance1-distance) > 3000) {
+      distance = max(distance1, distance);
+    }
   }
 
   long isInRange(int range) {
