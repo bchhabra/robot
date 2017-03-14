ed7c37cdbf0e04e0ca5cdb74406dc0291a555092
Nicolae Hodos <nicu.hodos@gmail.com>
2017-03-14 20:09:31 +0100

    remove the delay, not needed

--- huzzah/init.lua
+++ huzzah/init.lua
@@ -3,12 +3,6 @@ local sda = 1 -- GPIO 5
 local scl = 2 -- GPIO 4
 i2c.setup(id, sda, scl, i2c.SLOW)
 
-local serialPin = 8
-gpio.mode(serialPin, gpio.OUTPUT)
-gpio.write(serialPin, gpio.LOW)
-tmr.delay(3000000)
-gpio.write(serialPin, gpio.HIGH)
-
 local s,err
 if file.open("server.lua") then
     s,err = pcall(function() dofile("server.lua") end)
